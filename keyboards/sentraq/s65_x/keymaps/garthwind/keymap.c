efb138fe2fe7429483348fc31cc179e46b21c08a
Rob Hundley <rwhundle@us.ibm.com>
2019-08-26 10:03:18 -0400

    Revert "Added function layer spacebar"
    This reverts commit b01710ccc29628ca878e95e2300ada9444949c91, a revert
    that was a mistake to make.

--- keyboards/sentraq/s65_x/keymaps/garthwind/keymap.c
+++ keyboards/sentraq/s65_x/keymaps/garthwind/keymap.c
@@ -1,33 +1,43 @@
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FL 1
-#define _UL 2
+#define _ML 0
+#define _WL 1
+#define _MF 2
+#define _WF 3
+#define _UL 4
 
 #define FL_CAPS LT(_FL, KC_CAPS) // Tap to toggle caps lock and hold to activate function layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: Main layer, swapped alt and GUI for Mac
    * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-   * │GE │1 !│2 @│3 #│4 $│5 %│6 ^│7 &│8 *│9 (│0 )│- _│= +│` ~│\ |│Del│
+   * │GE │1 !│2 @│3 #│4 $│5 %│6 ^│7 &│8 *│9 (│0 )│- _│= +│` ~│\ |│Hom│
    * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┼───┤
-   * │Tab  │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │[ {│] }│Bspc │End│
+   * │Tab  │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │[ {│] }│Bspc │PUp│
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
-   * │Ctrl  │ A │ S │ D │ F │ G │ H │ J │ K │ L │; :│' "│Enter   │PUp│
+   * │Ctrl  │ A │ S │ D │ F │ G │ H │ J │ K │ L │; :│' "│Enter   │PDn│
    * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
-   * │Shift   │ Z │ X │ C │ V │ B │ N │ M │, <│. >│/ ?│Shift │Up │PDn│
+   * │Shift   │ Z │ X │ C │ V │ B │ N │ M │, <│. >│/ ?│Shift │Up │End│
    * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
-   * │Fn  │GUI │Alt │Space                   │Alt│Fn │Ctl│Lft│Dwn│Rgt│
+   * │Ctrl│GUI │Alt │Space(_FL)              │Alt│Fn │Ctl│Lft│Dwn│Rgt│
    * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
    */
 
   /* 0: ANSI qwerty */
-  [_BL] = LAYOUT_65_ansi_plus(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_DEL,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_HOME,  \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    MO(_UL), KC_LGUI, KC_LALT,                   LT(1, KC_SPC),                         KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
+  [_ML] = LAYOUT_65_ansi_plus(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_HOME, \
+    ALT_T(KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,  \
+    GUI_T(KC_CAPS),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN, \
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,  \
+    KC_LCTL, KC_LALT, KC_LGUI,                   LT(_MF, KC_SPC),                             DF(_WL), KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT  \
+  ),
+
+  [_WL] = LAYOUT_65_ansi_plus(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_HOME, \
+    ALT_T(KC_TAB),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,  \
+    CTL_T(KC_CAPS),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN, \
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,  \
+    KC_LCTL, KC_LGUI, KC_LALT,                   LT(_WF, KC_SPC),                             KC_RALT, DF(_ML), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
 
@@ -37,19 +47,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┼───┤
    * │     │   │Up │   │   │VUp│   │   │Ins│Del│   │   │   │     │   │
    * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
-   * │      │Lft│Dwn│Rgt│   │VDn│Hme│PgU│PgD│End│PSc│   │        │   │
+   * │      │Lft│Dwn│Rgt│   │VDn│Hom│PgD│PgU│End│PSc│   │        │   │
    * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
-   * │        │   │   │   │   │Mut│   │   │   │   │   │      │   │   │
+   * │        │   │   │   │Mut│Rst│   │   │   │   │   │      │   │   │
    * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
    * │    │    │    │                        │   │   │   │   │   │   │
    * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
    */
 
-  [_FL] = LAYOUT_65_ansi_plus(
-    _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
-    _______, _______, KC_UP,   _______, _______, KC_VOLU, _______, _______, KC_INS,  KC_DEL,  _______, _______, _______, _______, RESET, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_VOLD, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_PSCR, _______,          _______, _______, \
-    _______,          _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, \
+  [_MF] = LAYOUT_65_ansi_plus(
+    C(G(KC_Q)),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
+    _______, _______, KC_UP,   _______, _______, KC_VOLU, _______, _______, KC_INS,  KC_DEL,  _______, _______, _______, _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_VOLD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,          _______, _______, \
+    _______,          _______, _______, _______, KC_MUTE, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______  \
+  ),
+
+  [_WF] = LAYOUT_65_ansi_plus(
+    G(KC_L), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
+    _______, _______, KC_UP,   _______, _______, KC_VOLU, _______, _______, KC_INS,  KC_DEL,  _______, _______, _______, _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_VOLD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,          _______, _______, \
+    _______,          _______, _______, _______, KC_MUTE, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______,                   _______,                            _______, _______, _______, _______, _______, _______  \
   ),
 
