a2aa53383ff88b2323cbecd759949b0b91a52694
garthwind <25403505+garthwind@users.noreply.github.com>
2021-01-25 09:54:12 -0500

    Add Spectacle macros to moonlander

--- keyboards/moonlander/keymaps/garthwind/keymap.c
+++ keyboards/moonlander/keymaps/garthwind/keymap.c
@@ -22,10 +22,11 @@
 #include "version.h"
 
 enum layers {
-    _BASE,  // default layer
+    _BASE,   // default layer
     _LOWER,  // symbols
     _RAISE,  // media keys
-    _ADJUST,
+    _ADJUST, // 
+    _SPECT,  // Spectacles hotkeys
 };
 
 enum custom_keycodes {
@@ -34,14 +35,24 @@ enum custom_keycodes {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define SPECT MO(_SPECT)
 #define GUIESC GUI_T(KC_ESC)
+#define FULLSCR G(KC_F)
+#define SNAPLFT A(G(KC_LEFT))
+#define SNAPRGT A(G(KC_RGHT))
+#define SNAPTOP A(G(KC_UP))
+#define SNAPBOT A(G(KC_DOWN))
+#define SNAPQ1 C(G(KC_RGHT))
+#define SNAPQ2 C(G(KC_LEFT))
+#define SNAPQ3 C(G(S(KC_LEFT)))
+#define SNAPQ4 C(G(S(KC_RGHT)))
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_moonlander(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,           _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        GUIESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,           _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,            KC_INS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME,           KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        GUIESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,            KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENTER,
         _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,            _______,           _______, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
                                             KC_SPC,  KC_BSPC, KC_LGUI,           KC_RALT,  KC_TAB, KC_SPC
@@ -71,6 +82,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         EEP_RST, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
+                                            _______, _______, _______,           _______, _______, _______
+    ),
+    [_SPECT] = LAYOUT_moonlander(
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
+        EEP_RST, _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
     ),
 };
